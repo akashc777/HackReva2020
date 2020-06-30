ffe85bf3a6f834fabb6c7adcae829aefa621d57c
akashc777 <contact@akash.page>
2020-06-30 23:35:45 +0530

    Updates

---
+++ precache-manifest.2bfc9630f7ff16407fb72b1a5e227c76.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b86e3c07bd846a82a88d61b5c1dffd1",
+    "url": "/HackReva2020/index.html"
+  },
+  {
+    "revision": "9625071eaf9226d1ad0a",
+    "url": "/HackReva2020/static/css/main.b3162d6d.chunk.css"
+  },
+  {
+    "revision": "9e25cd6805473d00bc09",
+    "url": "/HackReva2020/static/js/2.f4459fd4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HackReva2020/static/js/2.f4459fd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9625071eaf9226d1ad0a",
+    "url": "/HackReva2020/static/js/main.2c7a3e01.chunk.js"
+  },
+  {
+    "revision": "9d5d4f5f192a0fb94bc7",
+    "url": "/HackReva2020/static/js/runtime-main.e8039f32.js"
+  },
+  {
+    "revision": "36899abf253a0451204bde91554e86de",
+    "url": "/HackReva2020/static/media/abstract3.36899abf.JPG"
+  },
+  {
+    "revision": "ee7abbe71778167e33469e7446d81675",
+    "url": "/HackReva2020/static/media/akash.ee7abbe7.jpeg"
+  },
+  {
+    "revision": "4078211fd8838fc45c6c5a79e5b4141f",
+    "url": "/HackReva2020/static/media/github.4078211f.png"
+  },
+  {
+    "revision": "286695f07bb66e109705a3efe66b32d5",
+    "url": "/HackReva2020/static/media/heart.286695f0.svg"
+  },
+  {
+    "revision": "93e6e77cf96690c785590e1217f98ec5",
+    "url": "/HackReva2020/static/media/hero.93e6e77c.JPG"
+  },
+  {
+    "revision": "ef4f95b8f43efb29b9a00f351fd07d19",
+    "url": "/HackReva2020/static/media/instagram.ef4f95b8.svg"
+  },
+  {
+    "revision": "e180e730ba0862db7987608ba3a7284c",
+    "url": "/HackReva2020/static/media/linkedin.e180e730.png"
+  },
+  {
+    "revision": "f8931e0a1e968a64a25b46be5b8709a2",
+    "url": "/HackReva2020/static/media/paper.f8931e0a.png"
+  },
+  {
+    "revision": "657203168cd6dc78c9e664629343a759",
+    "url": "/HackReva2020/static/media/telegram.65720316.png"
+  },
+  {
+    "revision": "e438ebb21234205ee52d53471aca2695",
+    "url": "/HackReva2020/static/media/twitter.e438ebb2.svg"
+  }
+]);
